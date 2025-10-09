1c5d52c6964134c6c1d5c76ee28d5efa9eccaadc
bacnm260 <learn.bacnm260@gmail.com>
2025-10-09 09:29:46 +0700

    complete home-view-novel-description

---
+++ src/components/novels/NovelDescription.vue
@@ -0,0 +1,91 @@
+<script setup>
+</script>
+
+<template>
+  <div class="novel-description">
+    <img alt="Novel cover" class="novel-cover" src="@/assets/mock-data/example.png" />
+
+    <div class="description-text">
+      <h3 class="title">Novel Title</h3>
+      <h4 class="author">Author Name</h4>
+      <p class="description">
+        This is a brief description of the novel. It provides an overview of the plot, characters, and setting to
+        entice readers to explore
+        further.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+      </p>
+
+      <div class="novel-status-block">
+        <span class="chapters">Chapters: 20</span>
+        <span class="status">Status: Completed</span>
+        <span class="progess">Progress: 5/20</span>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<style lang="css" scoped>
+.novel-description {
+
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 10px;
+  align-items: flex-start;
+
+  padding: 10px;
+  box-sizing: border-box;
+
+
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  background-color: #ffffff;
+}
+
+.novel-cover {
+  height: 100%;
+  width: auto;
+
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.description-text {
+  height: 100%;
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+h3,
+h4 {
+  height: auto;
+  box-sizing: border-box;
+  margin: 0px;
+
+}
+
+.description {
+  margin: 20px 0px;
+  height: auto;
+
+  display: -webkit-box;
+  -webkit-line-clamp: 6;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+
+}
+
+.novel-status-block {
+  margin-top: auto;
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
--- src/views/NovelsHomeView.vue
+++ src/views/NovelsHomeView.vue
@@ -1,4 +1,6 @@
 <script setup>
+import NovelDescription from '@/components/novels/NovelDescription.vue';
+
 
 </script>
 
@@ -6,9 +8,57 @@
 
   <div class="novels-home-view">
 
-    <h1>Novels Home View</h1>
+    <div class="history-container">
+      <h2>History</h2>
+
+      <ul class="history-list">
+        <li class="history-item" v-for="n in 10" :key="n">Novel {{ n }}</li>
+      </ul>
+
+    </div>
+
+    <NovelDescription class="description-container" />
 
   </div>
+
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.novels-home-view {
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.history-list {
+  height: 400px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+}
+
+.description-container {
+  height: 250px;
+  max-width: 50%;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  align-items: flex-start;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);
+}
+
+@media screen and (max-width: 930px) {
+  .description-container {
+    display: none;
+  }
+
+}
+</style>
